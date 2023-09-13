87fbaa90360e6ac05ccdb86eac7aa773d4ab4ce9
Blackbalrog <45615452+Blackbalrog@users.noreply.github.com>
2023-09-14 01:52:33 +0200

    Update UiManager
    Add center Window

--- src/main/java/fr/blackbalrog/pterofx/ui/UiManager.java
+++ src/main/java/fr/blackbalrog/pterofx/ui/UiManager.java
@@ -1,6 +1,8 @@
 package fr.blackbalrog.pterofx.ui;
 
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 /**
@@ -18,9 +20,16 @@ public class UiManager extends Application {
      * @throws Exception if something goes wrong
      */
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage)
+    {
         this.window = new Window(stage);
         this.window.setTitle("Hello World!");
+
+        //Center Window
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        this.window.getStage().setX((primScreenBounds.getWidth() - this.window.getStage().getWidth()) / 2);
+        this.window.getStage().setY((primScreenBounds.getHeight() - this.window.getStage().getHeight()) / 2);
+
         this.window.show();
     }
 }
